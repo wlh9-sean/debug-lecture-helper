24acb75edf7963d53ef8c8a267564f38ee0458b9
Joe Blank <joe.blank@live.com>
2018-12-29 18:49:03 -0800

    master branch setup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      count: 0
     }
   }
 
-  componentDidMount() {
-    console.log('mounted')
-    window.removeEventListener('error', () => {
-      console.log('removed')
-    }, true)
-    window.removeEventListener('unhandlerejection', () => {
-      console.log('removed')
-    }, true)
-  }
 
-  increment() {
-    this.setState({ count: this.state.count + 1 })
-  }
   render() {
     return (
       <div className="App">
-        <button onClick={() => this.incremen()}>Click Me!</button>
-        <p>
-          Button Clicks: {this.state.count}
-        </p>
+        <p>Checkout to branch "one" to start.</p>
       </div>
     );
   }
