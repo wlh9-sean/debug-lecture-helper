67014e11fc32f3b60cebfb776c6ff3933c2bb0bd
Joe Blank <joe.blank@live.com>
2018-12-29 17:39:27 -0800

    ejection test

--- src/App.js
+++ src/App.js
@@ -3,23 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  componentDidMount() {
+    console.log('mounted')
+    window.removeEventListener('error', () => {
+      console.log('removed')
+    }, true)
+    window.removeEventListener('unhandlerejection', () => {
+      console.log('removed')
+    }, true)
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={() => this.incremen()}>Click Me!</button>
+        <p>
+          Button Clicks: {this.state.count}
+        </p>
       </div>
     );
   }
