d225f64c71bedf0101cd43a195cb656b6fe15f14
Joe Blank <joe.blank@live.com>
2019-01-09 01:27:30 -0700

    number bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      result: null,
+      firstNum: null,
+      secondNum: null
     }
+    console.log('hey')
   }
 
+  calculate() {
+    let { firstNum, secondNum } = this.state;
+    let sum = firstNum + secondNum;
+    this.setState({
+      result: sum,
+      firstNum: '',
+      secondNum: ''
+    })
+    console.log('hey')
+  }
 
   render() {
     return (
       <div className="App">
-        <p>Checkout to branch "one" to start.</p>
+        <h1>Let's do some math!</h1>
+        <br />
+        <input
+          value={this.state.firstNum}
+          type="number"
+          onChange={(e) => this.setState({ firstNum: e.target.value })} />
+        <span>+</span>
+        <input
+          value={this.state.secondNum}
+          type="number"
+          onChange={(e) => this.setState({ secondNum: e.target.value })} />
+        <br /><br />
+        <button onClick={() => this.calculate()}>Calculate</button>
+        {
+          this.state.result ? (
+            <p>Result is {this.state.result}</p>
+          ) : null
+        }
       </div>
     );
   }
